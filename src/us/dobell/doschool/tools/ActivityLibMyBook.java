1ea55220b6cf060b50f2c597611d08c78b8b28ca
ForBetter <624955391@qq.com>
2014-01-01 19:55:39 +0800

    修改

--- src/us/dobell/doschool/tools/ActivityLibMyBook.java
+++ src/us/dobell/doschool/tools/ActivityLibMyBook.java
@@ -1,4 +1,5 @@
 package us.dobell.doschool.tools;
+
 import java.util.List;
 
 import us.dobell.doschool.R;
@@ -17,49 +18,48 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ActivityLibMyBook extends Activity {
+public class ActivityLibMyBook extends Activity{
 
 	private ImageButton left;
 	private TextView title;
 	private ListView listView;
 	private Handler handler;
-	private List<List<String>> data, data2;
+	private List<List<String>> data,data2;
 	private LayoutInflater inflater;
 	TextView text;
-
-	public void back(View view) {
+	
+	public void back(View view){
 		finish();
 	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tools_activity_lib_mybook);
-		left = (ImageButton) findViewById(R.id.tools_top_leftbutton);
-		title = (TextView) findViewById(R.id.tools_top_text);
-		title.setText("鎴戠殑涔︾睄");
-		listView = (ListView) findViewById(R.id.tools_activity_lib_mybook_list);
+		left = (ImageButton)findViewById(R.id.tools_top_leftbutton);
+		title = (TextView)findViewById(R.id.tools_top_text);
+		title.setText("我的书籍");
+		listView = (ListView)findViewById(R.id.tools_activity_lib_mybook_list);
 		handler = new Handler();
 		inflater = LayoutInflater.from(this);
-		// listView.setAdapter(new MyListAdapter());
+		//listView.setAdapter(new MyListAdapter());
 		init();
 	}
-
-	private void init() {
-		new Thread() {
-			public void run() {
-				data = MRemoteServer
-						.getMyBook(1, Values.User
-								.getCurrentUserFunId(ActivityLibMyBook.this),
-								ActivityLibMyBook.this.getIntent()
-										.getStringExtra("password"),
-								getApplicationContext(), handler);
-
+	
+	private void init(){
+		new Thread(){
+			public void run(){
+				data = MRemoteServer.getMyBook(1,
+						Values.User.getCurrentUserFunId(ActivityLibMyBook.this),
+						ActivityLibMyBook.this.getIntent().getStringExtra("password"),
+						getApplicationContext(),
+						handler
+						);
+				
 				handler.post(new Runnable() {
-
+					
 					@Override
 					public void run() {
-						if (data == null) {
+						if(data==null){
 							finish();
 							return;
 						}
@@ -69,48 +69,47 @@ public class ActivityLibMyBook extends Activity {
 							@Override
 							public void onItemClick(AdapterView<?> arg0,
 									View arg1, int arg2, long arg3) {
-								if (arg2 == data.size()) {
-									text.setText("姝ｅ湪鍔犺浇");
-									new Thread() {
-										public void run() {
-											data2 = MRemoteServer.getMyBook(
-													2,
-													Values.User
-															.getCurrentUserFunId(ActivityLibMyBook.this),
-													ActivityLibMyBook.this
-															.getIntent()
-															.getStringExtra(
-																	"password"),
+								if(arg2==data.size()){
+									text.setText("正在加载");
+									new Thread(){
+										public void run(){
+											data2 = MRemoteServer.getMyBook(2,
+													Values.User.getCurrentUserFunId(ActivityLibMyBook.this),
+													ActivityLibMyBook.this.getIntent().getStringExtra("password"),
 													getApplicationContext(),
-													handler);
-
+													handler
+													);
+											
 											handler.post(new Runnable() {
 												@Override
 												public void run() {
 													listView.setAdapter(new MyListAdapter());
-													text.setText("鏇村璁板綍");
+													text.setText("更多记录");
 												}
 											});
-
+											
 										}
 									}.start();
-
+									
+									
 								}
 							}
 						});
 					}
 				});
-
+				
 			}
 		}.start();
-
+		
+		
+		
 	}
-
-	class MyListAdapter extends BaseAdapter {
+	
+	class MyListAdapter extends BaseAdapter{
 
 		@Override
 		public int getCount() {
-			return data.size() + 1 + (data2 == null ? 0 : data2.size());
+			return data.size()+1+(data2==null?0:data2.size());
 		}
 
 		@Override
@@ -125,37 +124,32 @@ public class ActivityLibMyBook extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			if (position < data.size()) {
+			if(position < data.size()){
 				View v = inflater.inflate(R.layout.tools_lib_mybook_item, null);
-				TextView t1 = (TextView) v
-						.findViewById(R.id.tools_lib_mybook_item_text1), t2 = (TextView) v
-						.findViewById(R.id.tools_lib_mybook_item_text2);
+				TextView t1 =(TextView) v.findViewById(R.id.tools_lib_mybook_item_text1),
+						 t2 =(TextView) v.findViewById(R.id.tools_lib_mybook_item_text2);
 				t1.setText(data.get(position).get(0));
 				t1.setTextColor(Color.BLUE);
-				t2.setText("褰掕繕鏃ユ湡锛� + data.get(position).get(4));
+				t2.setText("归还日期："+data.get(position).get(4));
 				return v;
-
+				
 			}
-			if (position == data.size()) {
-				// ImageView v = new ImageView(ActivityLibMyBook.this);
-				View v = inflater.inflate(
-						R.layout.tools_activity_lib_mybook_more, null);
-				text = (TextView) v
-						.findViewById(R.id.tools_lib_mybook_more_text);
-				text.setText("鏇村璁板綍");
+			if(position == data.size()){
+				//ImageView v = new ImageView(ActivityLibMyBook.this);
+				View v = inflater.inflate(R.layout.tools_activity_lib_mybook_more, null);
+				text = (TextView)v.findViewById(R.id.tools_lib_mybook_more_text);
+				text.setText("更多记录");
 				return v;
 			}
-			if (position > data.size()) {
+			if(position > data.size()){
 				View v = inflater.inflate(R.layout.tools_lib_mybook_item, null);
-				TextView t1 = (TextView) v
-						.findViewById(R.id.tools_lib_mybook_item_text1), t2 = (TextView) v
-						.findViewById(R.id.tools_lib_mybook_item_text2);
-				t1.setText(data2.get(position - data.size() - 1).get(0));
+				TextView t1 =(TextView) v.findViewById(R.id.tools_lib_mybook_item_text1),
+						 t2 =(TextView) v.findViewById(R.id.tools_lib_mybook_item_text2);
+				t1.setText(data2.get(position-data.size()-1).get(0));
 				t1.setTextColor(Color.BLUE);
-				t2.setText("褰掕繕鏃ユ湡锛�
-						+ data2.get(position - data.size() - 1).get(4));
+				t2.setText("归还日期："+data2.get(position-data.size()-1).get(4));
 				return v;
-
+				
 			}
 			return null;
 		}
@@ -163,11 +157,13 @@ public class ActivityLibMyBook extends Activity {
 		@Override
 		public boolean isEnabled(int position) {
 			// TODO Auto-generated method stub
-			if (position == data.size())
+			if(position == data.size())
 				return true;
 			return false;
 		}
 
+		
+		
 	}
-
+	
 }
